362c567cd559cf0793e68dfcde31e0ad66784b5f
Graham <graham.a.ross@gmail.com>
2019-12-19 11:02:30 -0500

    Added back support for Blynk

--- code/ESP32/ClassicMQTT/src/main.cpp
+++ code/ESP32/ClassicMQTT/src/main.cpp
@@ -5,6 +5,7 @@
 #include <ArduinoJson.h>
 #include "AsyncMqttClient.h"
 #include <esp32ModbusTCP.h>
+#include <BlynkSimpleEsp32.h>
 #include "Log.h"
 #include "ChargeControllerInfo.h"
 
@@ -12,8 +13,9 @@
 #define WAKE_PUBLISH_RATE 5000
 #define SNOOZE_PUBLISH_RATE 300000
 #define WAKE_COUNT 60
-#define CONFIG_VERSION "V1.2"
-#define CONFIG_LEN 32  
+#define CONFIG_VERSION "V1.2.1"
+#define CONFIG_LEN 32
+#define BLYNK_PRINT Serial // Enables Serial Monitor
 
 AsyncMqttClient _mqttClient;
 TimerHandle_t mqttReconnectTimer;
@@ -30,13 +32,16 @@ char _mqttUserName[IOTWEBCONF_WORD_LEN];
 char _mqttUserPassword[IOTWEBCONF_WORD_LEN];
 char _mqttRootTopic[IOTWEBCONF_WORD_LEN];
 char _willTopic[IOTWEBCONF_WORD_LEN];
+char _blynkToken[IOTWEBCONF_WORD_LEN];
 IotWebConfParameter classicIPParam = IotWebConfParameter("Classic IP", "classicIP", _classicIP, IOTWEBCONF_WORD_LEN);
-IotWebConfParameter classicPortParam = IotWebConfParameter("Classic port", "classicPort", _classicPort, 5, "text", NULL, "8080");
-IotWebConfSeparator seperatorParam = IotWebConfSeparator("MQTT");
+IotWebConfParameter classicPortParam = IotWebConfParameter("Classic port", "classicPort", _classicPort, 5, "text", NULL, "502");
+IotWebConfSeparator MQTT_seperatorParam = IotWebConfSeparator("MQTT");
 IotWebConfParameter mqttServerParam = IotWebConfParameter("MQTT server", "mqttServer", _mqttServer, IOTWEBCONF_WORD_LEN);
-IotWebConfParameter mqttPortParam = IotWebConfParameter("MQTT port", "mqttSPort", _mqttPort, 5, "text", NULL, "8080");
+IotWebConfParameter mqttPortParam = IotWebConfParameter("MQTT port", "mqttSPort", _mqttPort, 5, "text", NULL, "1883");
 IotWebConfParameter mqttUserNameParam = IotWebConfParameter("MQTT user", "mqttUser", _mqttUserName, IOTWEBCONF_WORD_LEN);
 IotWebConfParameter mqttUserPasswordParam = IotWebConfParameter("MQTT password", "mqttPass", _mqttUserPassword, IOTWEBCONF_WORD_LEN, "password");
+IotWebConfSeparator Blynk_seperatorParam = IotWebConfSeparator("Blynk");
+IotWebConfParameter blynkTokenParam = IotWebConfParameter("Blynk token", "blinkToken", _blynkToken, IOTWEBCONF_WORD_LEN);
 
 unsigned long _lastPublishTimeStamp = 0;
 unsigned long _lastModbusPollTimeStamp = 0;
@@ -48,19 +53,46 @@ bool mqttReadingsAvailable = false;
 bool boilerPlateInfoPublished = false;
 uint8_t boilerPlateReadBitField = 0;
 ChargeControllerInfo _chargeControllerInfo;
-esp32ModbusTCP* _pClassic;
+esp32ModbusTCP *_pClassic;
 
 int _currentRegister = 0;
 
-#define numBanks (sizeof(_registers)/sizeof(ModbusRegisterBank))
+#define numBanks (sizeof(_registers) / sizeof(ModbusRegisterBank))
 ModbusRegisterBank _registers[] = {
-	{ false, 4100, 44 },
-	{ false, 4360, 22 },
-	{ false, 4163, 2 },
-	{ false, 4209, 4 },
-	{ false, 4243, 32 }
-//{ false, 16386, 8 }
+	{false, 4100, 44},
+	{false, 4360, 22},
+	{false, 4163, 2},
+	{false, 4209, 4},
+	{false, 4243, 32}
+	//{ false, 16386, 8 }
 };
+
+BLYNK_READ(V0)
+{
+	Blynk.virtualWrite(V0, _chargeControllerInfo.BatVoltage);
+}
+
+BLYNK_READ(V1)
+{
+	Blynk.virtualWrite(V1, _chargeControllerInfo.BatCurrent);
+}
+
+BLYNK_READ(V2)
+{
+	Blynk.virtualWrite(V2, _chargeControllerInfo.SOC);
+}
+BLYNK_READ(V3)
+{
+	Blynk.virtualWrite(V3, _chargeControllerInfo.Power);
+}
+BLYNK_READ(V4)
+{
+	Blynk.virtualWrite(V4, _chargeControllerInfo.PVVoltage);
+}
+BLYNK_READ(V5)
+{
+	Blynk.virtualWrite(V5, _chargeControllerInfo.PVCurrent);
+}
 
 void publish(const char *subtopic, const char *value, boolean retained = false)
 {
@@ -73,11 +105,13 @@ void publish(const char *subtopic, const char *value, boolean retained = false)
 }
 
 // note, add 0.01 as a work around for Android JSON deserialization bug with float
-void publishReadings() {
+void publishReadings()
+{
 	StaticJsonDocument<1024> root;
-	if ((boilerPlateReadBitField & 0x0f) == 0x0f && boilerPlateInfoPublished == false) {
+	if ((boilerPlateReadBitField & 0x0f) == 0x0f && boilerPlateInfoPublished == false)
+	{
 		boilerPlateInfoPublished = true;
-		 
+
 		root["appVersion"] = _chargeControllerInfo.appVersion;
 		root["buildDate"] = _chargeControllerInfo.buildDate;
 		root["deviceName"] = _chargeControllerInfo.deviceName;
@@ -125,42 +159,52 @@ void publishReadings() {
 	root["PCBTemperature"] = _chargeControllerInfo.PCBTemperature + 0.01;
 	String s;
 	serializeJson(root, s);
-	publish("readings",  s.c_str());
+	publish("readings", s.c_str());
 }
 
-float GetFloatValue(int index, uint8_t* data, float div = 1.0) {
-   index *= 2;
-   int16_t temp = (data[index] << 8 | data[index + 1]);
-   return  temp/div;
+float GetFloatValue(int index, uint8_t *data, float div = 1.0)
+{
+	index *= 2;
+	int16_t temp = (data[index] << 8 | data[index + 1]);
+	return temp / div;
 }
 
-uint16_t Getint16Value(int index, uint8_t* data) {
+uint16_t Getint16Value(int index, uint8_t *data)
+{
 	index *= 2;
 	return (data[index] << 8 | data[index + 1]);
 }
 
-uint32_t Getint32Value(int index, uint8_t* data) {
+uint32_t Getint32Value(int index, uint8_t *data)
+{
 	index *= 2;
 	return data[index + 2] << 24 | data[index + 3] << 16 | data[index] << 8 | data[index + 1];
 }
 
-uint8_t GetMSBValue(int index, uint8_t* data) {
+uint8_t GetMSBValue(int index, uint8_t *data)
+{
 	index *= 2;
 	return (data[index] >> 8);
 }
 
-boolean GetFlagValue(int index, uint16_t mask, uint8_t* data) {
+boolean GetFlagValue(int index, uint16_t mask, uint8_t *data)
+{
 	index *= 2;
 	return (data[index] & mask) != 0;
 }
 
-void readModbus() {
-	if (_currentRegister < numBanks) {
-		if (_registers[_currentRegister].received == false) {
-			if (_pClassic->readHoldingRegisters(_registers[_currentRegister].address, _registers[_currentRegister].byteCount) != 0) {
+void readModbus()
+{
+	if (_currentRegister < numBanks)
+	{
+		if (_registers[_currentRegister].received == false)
+		{
+			if (_pClassic->readHoldingRegisters(_registers[_currentRegister].address, _registers[_currentRegister].byteCount) != 0)
+			{
 				logi("Requesting %d for %d bytes\n", _registers[_currentRegister].address, _registers[_currentRegister].byteCount);
 			}
-			else {
+			else
+			{
 				loge("Request %d failed\n", _registers[_currentRegister].address);
 			}
 		}
@@ -168,10 +212,13 @@ void readModbus() {
 	}
 }
 
-void SetBankReceived(uint16_t byteCount) {
+void SetBankReceived(uint16_t byteCount)
+{
 	int regCount = byteCount / 2;
-	for (int i = 0; i < numBanks; i++) {
-		if (_registers[i].byteCount == regCount) {
+	for (int i = 0; i < numBanks; i++)
+	{
+		if (_registers[i].byteCount == regCount)
+		{
 			_registers[i].received = true;
 		}
 	}
@@ -184,7 +231,8 @@ void SetBankReceived(uint16_t byteCount) {
 //	}
 //}
 
-void Wake() {
+void Wake()
+{
 	_publishRate = WAKE_PUBLISH_RATE;
 	_lastPublishTimeStamp = 0;
 	_lastModbusPollTimeStamp = 0;
@@ -193,58 +241,60 @@ void Wake() {
 void modbusErrorCallback(uint16_t packetId, MBError error)
 {
 	String text;
-	switch (error) 
+	switch (error)
 	{
-		case 0x00:
+	case 0x00:
 		text = "SUCCESS";
 		break;
-		case 0x01:
+	case 0x01:
 		text = "ILLEGAL_FUNCTION";
 		break;
-		case 0x02:
+	case 0x02:
 		text = "ILLEGAL_DATA_ADDRESS";
 		break;
-		case 0x03:
+	case 0x03:
 		text = "ILLEGAL_DATA_VALUE";
 		break;
-		case 0x04:
+	case 0x04:
 		text = "SERVER_DEVICE_FAILURE";
 		break;
-		case 0x05:
+	case 0x05:
 		text = "ACKNOWLEDGE";
 		break;
-		case 0x06:
+	case 0x06:
 		text = "SERVER_DEVICE_BUSY";
 		break;
-		case 0x07:
+	case 0x07:
 		text = "NEGATIVE_ACKNOWLEDGE";
 		break;
-		case 0x08:
+	case 0x08:
 		text = "MEMORY_PARITY_ERROR";
 		break;
-		case 0xE0:
+	case 0xE0:
 		text = "TIMEOUT";
 		break;
-		case 0xE1:
+	case 0xE1:
 		text = "INVALID_SLAVE";
 		break;
-		case 0xE2:
+	case 0xE2:
 		text = "INVALID_FUNCTION";
 		break;
-		case 0xE3:
+	case 0xE3:
 		text = "CRC_ERROR";
 		break;
-		case 0xE4:
+	case 0xE4:
 		text = "COMM_ERROR";
 		break;
 	}
 	loge("packetId[0x%x], error[%s]", packetId, text);
 }
 
-void modbusCallback(uint16_t packetId, uint8_t slaveAddress, MBFunctionCode functionCode, uint8_t* data, uint16_t byteCount) {
+void modbusCallback(uint16_t packetId, uint8_t slaveAddress, MBFunctionCode functionCode, uint8_t *data, uint16_t byteCount)
+{
 	logd("packetId[0x%x], slaveAddress[0x%x], functionCode[0x%x], byteCount[%d]", packetId, slaveAddress, functionCode, byteCount);
 	SetBankReceived(byteCount);
-	if (byteCount == 88) {
+	if (byteCount == 88)
+	{
 		_chargeControllerInfo.BatVoltage = GetFloatValue(14, data, 10.0);
 		_chargeControllerInfo.PVVoltage = GetFloatValue(15, data, 10.0);
 		_chargeControllerInfo.BatCurrent = GetFloatValue(16, data, 10.0);
@@ -262,8 +312,9 @@ void modbusCallback(uint16_t packetId, uint8_t slaveAddress, MBFunctionCode func
 		_chargeControllerInfo.EqualizeTime = Getint16Value(42, data);
 		_chargeControllerInfo.Aux1 = GetFlagValue(29, 0x4000, data);
 		_chargeControllerInfo.Aux2 = GetFlagValue(29, 0x8000, data);
-		
-		if ((boilerPlateReadBitField & 0x1) == 0) {
+
+		if ((boilerPlateReadBitField & 0x1) == 0)
+		{
 			boilerPlateReadBitField |= 0x1;
 			uint16_t reg1 = Getint16Value(0, data);
 			char buf[32];
@@ -277,7 +328,8 @@ void modbusCallback(uint16_t packetId, uint8_t slaveAddress, MBFunctionCode func
 			_chargeControllerInfo.unitID = Getint32Value(10, data);
 		}
 	}
-	else if (byteCount == 44) { // whizbang readings
+	else if (byteCount == 44)
+	{ // whizbang readings
 		_chargeControllerInfo.PositiveAmpHours = Getint32Value(4, data);
 		_chargeControllerInfo.NegativeAmpHours = abs(Getint32Value(6, data));
 		_chargeControllerInfo.NetAmpHours = 0; //Getint32Value(8, data); // todo causing deserialization exception in android
@@ -287,16 +339,20 @@ void modbusCallback(uint16_t packetId, uint8_t slaveAddress, MBFunctionCode func
 		_chargeControllerInfo.RemainingAmpHours = Getint16Value(16, data);
 		_chargeControllerInfo.TotalAmpHours = Getint16Value(20, data);
 	}
-	else if (byteCount == 4) { // boilerplate data
-		if ((boilerPlateReadBitField & 0x02) == 0) {
+	else if (byteCount == 4)
+	{ // boilerplate data
+		if ((boilerPlateReadBitField & 0x02) == 0)
+		{
 			boilerPlateReadBitField |= 0x02;
 			_chargeControllerInfo.mpptMode = Getint16Value(0, data);
 			int Aux12FunctionS = (Getint16Value(1, data) & 0x3f00) >> 8;
 			_chargeControllerInfo.hasWhizbang = Aux12FunctionS == 18;
 		}
 	}
-	else if (byteCount == 8) { 
-		if ((boilerPlateReadBitField & 0x04) == 0) {
+	else if (byteCount == 8)
+	{
+		if ((boilerPlateReadBitField & 0x04) == 0)
+		{
 			boilerPlateReadBitField |= 0x04;
 			char unit[9];
 			unit[0] = data[1];
@@ -311,8 +367,10 @@ void modbusCallback(uint16_t packetId, uint8_t slaveAddress, MBFunctionCode func
 			_chargeControllerInfo.deviceName = unit;
 		}
 	}
-	else if (byteCount == 64) {
-		if ((boilerPlateReadBitField & 0x08) == 0) {
+	else if (byteCount == 64)
+	{
+		if ((boilerPlateReadBitField & 0x08) == 0)
+		{
 			boilerPlateReadBitField |= 0x08;
 			_chargeControllerInfo.VbattRegSetPTmpComp = GetFloatValue(0, data, 10.0);
 			_chargeControllerInfo.nominalBatteryVoltage = Getint16Value(1, data);
@@ -320,8 +378,10 @@ void modbusCallback(uint16_t packetId, uint8_t slaveAddress, MBFunctionCode func
 			_chargeControllerInfo.ReasonForResting = Getint16Value(31, data);
 		}
 	}
-	else if (byteCount == 16) {
-		if ((boilerPlateReadBitField & 0x10) == 0) {
+	else if (byteCount == 16)
+	{
+		if ((boilerPlateReadBitField & 0x10) == 0)
+		{
 			boilerPlateReadBitField |= 0x10;
 			short reg16387 = Getint16Value(0, data);
 			short reg16388 = Getint16Value(1, data);
@@ -360,7 +420,17 @@ void connectToMqtt()
 	logi("Connecting to MQTT...");
 	if (WiFi.isConnected())
 	{
-		_mqttClient.connect();
+		if (strlen(_mqttServer) > 0) // mqtt configured
+		{
+			_mqttClient.connect();
+		}
+		if (!Blynk.connected())
+		{
+			if (strlen(_blynkToken) > 0)
+			{
+				Blynk.connect();
+			}
+		}
 	}
 }
 
@@ -376,8 +446,16 @@ void handleRoot()
 		return;
 	}
 	String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
-	s += "<title>ESPThermostat</title></head><body>";
+	s += "<title>ClassicMQTT</title></head><body>";
 	s += _iotWebConf.getThingName();
+	s += "<ul>";
+	s += "<li>Classic IP: ";
+	s += _classicIP;
+	s += "</ul>";
+	s += "<ul>";
+	s += "<li>Classic port: ";
+	s += _classicPort;
+	s += "</ul>";
 	s += "<ul>";
 	s += "<li>MQTT server: ";
 	s += _mqttServer;
@@ -394,6 +472,10 @@ void handleRoot()
 	s += "<li>MQTT root topic: ";
 	s += _mqttRootTopic;
 	s += "</ul>";
+	s += "<ul>";
+	s += "<li>Blynk token: ";
+	s += _blynkToken;
+	s += "</ul>";
 	s += "Go to <a href='config'>configure page</a> to change values.";
 	s += "</body></html>\n";
 
@@ -408,11 +490,15 @@ void configSaved()
 boolean formValidator()
 {
 	boolean valid = true;
-	int l = _webServer.arg(mqttServerParam.getId()).length();
-	if (l < 3)
+	int mqttServerParamLength = _webServer.arg(mqttServerParam.getId()).length();
+	int blynkTokenParamLength = _webServer.arg(blynkTokenParam.getId()).length();
+	if (mqttServerParamLength == 0)
 	{
-		mqttServerParam.errorMessage = "Please provide at least 3 characters!";
-		valid = false;
+		if (blynkTokenParamLength == 0)
+		{
+			mqttServerParam.errorMessage = "MQTT server or Blynk token is required";
+			valid = false;
+		}
 	}
 	return valid;
 }
@@ -449,21 +535,25 @@ void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties 
 	{
 		pl[i] = toupper(payload[i]);
 	}
-	if (strncmp(pl, "{\"WAKE\"}", len) == 0) {
+	if (strncmp(pl, "{\"WAKE\"}", len) == 0)
+	{
 		boilerPlateInfoPublished = false;
 		Wake();
 		logd("Wake poll rate");
 	}
-	if (strncmp(pl, "{\"INFO\"}", len) == 0) {
+	if (strncmp(pl, "{\"INFO\"}", len) == 0)
+	{
 		boilerPlateInfoPublished = false;
 		Wake();
 		logd("info request received");
 	}
 }
 
-void setup() {
+void setup()
+{
 	Serial.begin(115200);
-	while (!Serial) {
+	while (!Serial)
+	{
 		; // wait for serial port to connect. Needed for native USB port only
 	}
 	logi("Booting");
@@ -476,11 +566,14 @@ void setup() {
 	// setup EEPROM parameters
 	_iotWebConf.addParameter(&classicIPParam);
 	_iotWebConf.addParameter(&classicPortParam);
-	_iotWebConf.addParameter(&seperatorParam);
+	_iotWebConf.addParameter(&MQTT_seperatorParam);
 	_iotWebConf.addParameter(&mqttServerParam);
 	_iotWebConf.addParameter(&mqttPortParam);
 	_iotWebConf.addParameter(&mqttUserNameParam);
 	_iotWebConf.addParameter(&mqttUserPasswordParam);
+	_iotWebConf.addParameter(&Blynk_seperatorParam);
+	_iotWebConf.addParameter(&blynkTokenParam);
+
 	// setup callbacks for IotWebConf
 	_iotWebConf.setConfigSavedCallback(&configSaved);
 	_iotWebConf.setFormValidator(&formValidator);
@@ -495,6 +588,7 @@ void setup() {
 		_mqttPort[0] = '\0';
 		_mqttUserName[0] = '\0';
 		_mqttUserPassword[0] = '\0';
+		_blynkToken[0] = '\0';
 		_iotWebConf.resetWifiAuthInfo();
 	}
 	else
@@ -520,6 +614,10 @@ void setup() {
 			_pClassic->onData(modbusCallback);
 			_pClassic->onError(modbusErrorCallback);
 		}
+		if (strlen(_blynkToken) > 0)
+		{
+			Blynk.config(_blynkToken);
+		}
 	}
 	// Set up required URL handlers on the web server.
 	_webServer.on("/", handleRoot);
@@ -532,29 +630,36 @@ void setup() {
 	logi("Done setup");
 }
 
-void loop() {
+void loop()
+{
 	_iotWebConf.doLoop();
-	if (_mqttClient.connected())
+	if (WiFi.isConnected())
 	{
-		if (_lastModbusPollTimeStamp < millis()) {
+		if (_lastModbusPollTimeStamp < millis())
+		{
 			_lastModbusPollTimeStamp = millis() + MODBUS_POLL_RATE;
 			readModbus();
-			if (_currentRegister >= numBanks) {
+			if (_currentRegister >= numBanks)
+			{
 				_currentRegister = 0;
 				_registers[0].received = false; // repeat readings
 				_registers[1].received = false;
 			}
 		}
-		if (_lastPublishTimeStamp < millis()) {
-			_lastPublishTimeStamp = millis() + _publishRate;
-			_publishCount++;
-			publishReadings();
+		if (_mqttClient.connected())
+		{
+			if (_lastPublishTimeStamp < millis())
+			{
+				_lastPublishTimeStamp = millis() + _publishRate;
+				_publishCount++;
+				publishReadings();
+			}
+			if (_publishCount >= WAKE_COUNT)
+			{
+				_publishCount = 0;
+				_publishRate = SNOOZE_PUBLISH_RATE;
+			}
 		}
-		if (_publishCount >= WAKE_COUNT) {
-			_publishCount = 0;
-			_publishRate = SNOOZE_PUBLISH_RATE;
-		}
+		Blynk.run();
 	}
 }
-
-
