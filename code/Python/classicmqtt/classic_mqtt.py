30870a4ada7f91a032b5321ba38fa891758b1f28
Matt Sargent <matthew.c.sargent@gmail.com>
2020-02-26 17:25:08 -0500

    Update classic_mqtt.py
    Added command line handling.

--- code/Python/classicmqtt/classic_mqtt.py
+++ code/Python/classicmqtt/classic_mqtt.py
@@ -7,6 +7,7 @@ import json
 import time
 import threading
 import logging
+import sys, getopt
 
 from classic_modbusdecoder import getRegisters, getDataDecoder, doDecode
 from classic_jsonencoder import encodeClassicData_readings, encodeClassicData_info
@@ -19,9 +20,7 @@ MODBUS_POLL_RATE          = 5                   #Get data from the Classic every
 MQTT_PUBLISH_RATE         = 5                   #Check to see if anything needs publishing every 5 seconds.
 MQTT_SNOOZE_COUNT         = 60                  #When no one is listening, publish every 5 minutes
 WAKE_COUNT                = 60                  #The number of times to publish after getting a "wake"
-MQTT_ROOT_DEFAULT         = "ClassicMATT"
 
-mqttRoot                  = MQTT_ROOT_DEFAULT
 classicModbusData         = dict()
 snoozeCount               = 0
 snoozing                  = True
@@ -29,8 +28,17 @@ wakeCount                 = 0
 infoPublished             = True
 doStop                    = False
 classicDataGood           = False
-mqttHost                  = "0.tcp.ngrok.io"
-mqttPort                  = 15284
+mqttConnected             = False
+
+
+classicHost               = "ClassicHost"
+classicPort               = "502" #15284
+mqttHost                  = "127.0.0.1"
+mqttPort                  = 1883
+mqttRoot                  = "ClassicMQTT"
+mqttUser                  = "username"
+mqttPassword              = "password"
+
 
 
 
@@ -50,7 +58,7 @@ def getModbusData():
 
     global classicDataGood
 
-    modclient = ModbusClient(mqttHost, port=mqttPort)
+    modclient = ModbusClient(classicHost, port=classicPort)
     #Test for succesful connect, if not, log error and mark modbusConnected = False
     modclient.connect()
     
@@ -83,11 +91,17 @@ def getModbusData():
     return decoded
 
 def on_connect(client, userdata, flags, rc):
+    global mqttConnected
     if rc==0:
+        mqttConnected = True
         log.debug("MQTT connected OK Returned code={}".format(rc))
     else:
+        mqttConnected = False
         log.error("MQTT Bad connection Returned code={}".format(rc))
 
+def on_disconnect(client, userdata, rc):
+    global mqttConnected
+    mqttConnected = False
 
 def on_message(client, userdata, message):
         #print("Received message '" + str(message.payload) + "' on topic '"
@@ -126,8 +140,11 @@ def mqttPublish(client, data, subtopic):
 
     topic = "{}/classic/stat/{}".format(mqttRoot, subtopic)
     log.debug(topic)
-    client.publish(topic,data)
+    
+    if not mqttConnected:
+        log.error("MQTT not connected, skipping publish")
 
+    client.publish(topic,data)
 
 def publish(client):
     global infoPublished, classicModbusData, classicDataGood
@@ -182,18 +199,72 @@ def mqtt_publish_periodic(mqtt_stop, client):
         # set myself to be called again in correct number of seconds
         threading.Timer(MQTT_PUBLISH_RATE, mqtt_publish_periodic, [mqtt_stop, client]).start()
 
+def handleArgs(argv):
+    
+    global classicHost, classicPort,mqttHost, mqttPort, mqttRoot, mqttUser, mqttPassword
 
-def run():
+    classic = classicHost
+    classic_port = classicPort
+    mqtt = mqttHost
+    mqtt_port = mqttPort
+    mqtt_root = mqttRoot
+    username = mqttUser
+    password = mqttPassword
 
-    global doStop, mqttRoot
+    try:
+      opts, args = getopt.getopt(argv,"h",["classic=","classic_port=","mqtt=","mqtt_port=","mqtt_root=","user=","pass="])
+    except getopt.GetoptError:
+        print ("classic_mqtt.py --classic <{}> --classic_port <{}> --mqtt <{}> --mqtt_port <{}> --mqtt_root <{}> --user <username> --pass <password>".format(classic, classic_port, mqtt, mqtt_port, mqtt_root))
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ("classic_mqtt.py --classic <{}> --classic_port <{}> --mqtt <{}> --mqtt_port <{}> --mqtt_root <{}> --user <username> --pass <password>".format(classic, classic_port, mqtt, mqtt_port, mqtt_root))
+            sys.exit()
+        elif opt in ('--classic'):
+            classic = arg
+        elif opt in ('--classic_port'):
+            classic_port = arg
+        elif opt in ("--mqtt"):
+            mqtt = arg
+        elif opt in ("--mqtt_port"):
+            mqtt_port = arg
+        elif opt in ("--mqtt_root"):
+            mqtt_root = arg
+        elif opt in ("--user"):
+            username = arg
+        elif opt in ("--pass"):
+            password = arg
+
+    log.debug("classic is {}".format(classic))
+    log.debug("classic_port is {}".format(classic_port))
+    log.debug("mqtt is {}".format(mqtt))
+    log.debug("mqtt_port is {}".format(mqtt_port))
+    log.debug("mqtt_root is {}".format(mqtt_root))
+    log.debug("username is {}".format(username))
+    log.debug("password is {}".format(password))
+
+    classicHost = classic
+    classicPort = classic_port
+    mqttHost = mqtt
+    mqttPort = mqtt_port
+    mqttRoot = mqtt_root
+    mqttUser = username
+    mqttPassword = password
+
+
+def run(argv):
+
+    handleArgs(argv)
+
+
+    global doStop, mqttRoot, mqttConnected
 
     #setup the MQTT Client for publishing and subscribing
-    broker_address="islandmqtt.eastus.cloudapp.azure.com"     
-    client = mqttclient.Client("Classic") #create new instance
-    client.username_pw_set("glaserisland", password="R@staman1312")
-    
+    client = mqttclient.Client(mqttUser+"_mqttclient") #create new instance
+    client.username_pw_set(mqttUser, password=mqttPassword)
     client.on_connect = on_connect    
-    client.connect(broker_address) #connect to broker
+    client.on_disconnect = on_disconnect   
+    client.connect(host=mqttHost,port=int(mqttPort)) #connect to broker islandmqtt.eastus.cloudapp.azure.com
     
     #setup command subscription
     client.on_message = on_message 
@@ -216,13 +287,22 @@ def run():
     mqtt_publish_periodic(mqtt_stop, client)
 
     keepon = True
+    mqttDiscCount = 0
+
     while keepon:
-        time.sleep(1)
+        time.sleep(5)
         #check to see if shutdown received
         if doStop:
             log.debug("Stopping...")
             keepon = False
-
+        elif not mqttConnected:
+            if (mqttDiscCount > 300):
+                log.error("MQTT Disconnected")
+                keepon = False
+            else:
+                mqttDiscCount = mqttDiscCount + 1
+        else:
+            mqttDiscCount = 0
     
     modbus_stop.set()
     mqtt_stop.set()
@@ -230,4 +310,4 @@ def run():
 
 
 if __name__ == '__main__':
-    run()
+    run(sys.argv[1:])
