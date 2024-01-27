1a3261a2693e75b30cca7f71ddf914adb3174f82
Frank Peeters <fastfrank.iphone@gmail.com>
2024-01-27 03:05:34 +0100

    Added Home-Assistant autodiscovery MQTT (#2)
    * Added Home-Assistant autodiscovery MQTT
    * Home-Assistant Auto Discovery with --homeassistant

--- code/Python/support/classic_jsonencoder.py
+++ code/Python/support/classic_jsonencoder.py
@@ -20,17 +20,22 @@ def encodeClassicData_readings(decoded):
     #log.debug("Enter encodeClassicData_readings")
 
     classicData = {}
-
+    
+    classicData["currentTime"] = decodeCTIME( decoded["CTIME0"],decoded["CTIME1"], decoded["CTIME2"] )
+    
     # "BatTemperature":-1.99,
     classicData["BatTemperature"] = decoded["BatTemperature"]
     # "NetAmpHours":0,
     classicData["NetAmpHours"] = decoded["WbJrAmpHourNET"]
     # "ChargeState":0,
     classicData["ChargeState"] = decoded["ChargeStage"] #it is mis-labeled in the ESP32 code
+    classicData["ChargeStateIcon"] = decoded["ChargeStateIcon"]
+    classicData["ChargeStateText"] = decoded["ChargeStateText"]
     # "InfoFlagsBits":-1308610300,
     classicData["InfoFlagsBits"] = decoded["InfoFlagsBits"]
     # "ReasonForResting":104,
     classicData["ReasonForResting"] = decoded["ReasonForResting"]
+    classicData["ReasonForRestingText"] = decoded["ReasonForRestingText"]
     # "NegativeAmpHours":-9170,
     classicData["NegativeAmpHours"] = decoded["WbJrAmpHourNEGative"]
     # "BatVoltage":25.21,
@@ -55,7 +60,8 @@ def encodeClassicData_readings(decoded):
     classicData["EqualizeTime"] = decoded["EqualizeTime"]
     # "SOC":99,
     classicData["SOC"] = decoded["SOC"]
-    # "Aux1":false,
+    classicData["SOCicon"] = decoded["SOCicon"]
+   # "Aux1":false,
     classicData["Aux1"] = ((decoded["InfoFlagsBits"] & 0x00004000) != 0)
     # "Aux2":false,
     classicData["Aux2"] = ((decoded["InfoFlagsBits"] & 0x00008000) != 0)
@@ -79,7 +85,7 @@ def encodeClassicData_readings(decoded):
     classicData["PCBTemperature"] = decoded["PCBTemperature"]
 
     return json.dumps(classicData, sort_keys=False, separators=(',', ':'))
-
+    
 # --------------------------------------------------------------------------- # 
 # Handle creating the Json for Info
 # --------------------------------------------------------------------------- # 
@@ -92,9 +98,8 @@ def encodeClassicData_info(decoded):
     #Assemble the string
     uint_array = [decoded["Name1"],decoded["Name0"],decoded["Name3"],decoded["Name2"],decoded["Name5"],decoded["Name4"],decoded["Name7"],decoded["Name6"]]
     # "deviceName":"CLASSIC",
-    classicData["deviceName"] = "".join(chr(x) for x in uint_array)
+    classicData["deviceName"] = "".join(chr(x) for x in uint_array).strip(chr(0))
     # "buildDate":"Tuesday, February 6, 2018",
-    bdate = datetime.date(decoded["Year"],decoded["Month"],decoded["Day"])
     classicData["buildDate"] = bdate.strftime("%A, %B %d, %Y").replace(' 0', ' ') # get rid of the stupid leading 0 in date.
     # "deviceType":"Classic",
     classicData["deviceType"] = "Classic"
@@ -119,4 +124,22 @@ def encodeClassicData_info(decoded):
     mac = "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}".format(decoded["mac_5"],decoded["mac_4"],decoded["mac_3"],decoded["mac_2"],decoded["mac_1"],decoded["mac_0"])
     classicData["macAddress"] = mac.upper()
 
+    classicData["IP"] = decoded["IP"]
+
     return json.dumps(classicData, sort_keys=False, separators=(',', ':'))
+
+def decodeCTIME( CTIME0, CTIME1, CTIME2):
+    
+    # CTIME0 - BITS 5:0 seconds 13:8 minutes 20:16 Hours 26:24 DayOfWeek
+    tsecs = (CTIME0 & 0x0000003F)
+    tmins = (CTIME0 & 0x00003F00) >> 8
+    thour = (CTIME0 & 0x001F0000) >> 16
+    tdyow = (CTIME0 & 0x07000000) >> 24
+    # CTIME1 - BITS 4:0 day of month 11:8 month 27:16 year
+    tdyom = (CTIME1 & 0x0000001F)
+    tmnth = (CTIME1 & 0x00000F00) >> 8
+    tyear = (CTIME1 & 0x0FFF0000) >> 16
+    # CTIME2 - BITS 11:0 day of year
+    tdyoy = (CTIME2 & 0x000007FF)    #  1FF
+    #
+    return "{:04n}-{:02n}-{:02n} {:02n}:{:02n}:{:02n}".format(tyear,tmnth,tdyom,thour,tmins,tsecs)
